0a3d358e395f2ac2b9815ca7cfeea5661fcb57de
David <davidbarbosa425@gmail.com>
2022-03-06 12:07:54 -0300

    Classe Usuário e UsuarioRepository

---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,39 @@
+﻿using eCommerce.API.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){Id = 1, Nome = "Axl Rose", Email = "gunsandroses@gmail.com"},
+            new Usuario(){Id = 2, Nome = "Slash", Email = "gunsandroses@gmail.com"},
+            new Usuario{Id = 3, Nome = "Duff Mackgan", Email = "gunsandroses@gmail.com"}
+        };
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(x => x.Id == id);
+        }
+        public void Insert(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+        public void Update(Usuario usuario)
+        {
+
+            usuario.Id = usuario.Id;
+            usuario.Nome = usuario.Nome;
+            usuario.Email = usuario.Email;
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
