0e3ee746c9db654e2f6ba8223e602e950b2fe272
David <davidbarbosa425@gmail.com>
2022-03-10 20:25:40 -0300

    pegas usuarios

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,10 +1,11 @@
-﻿using eCommerce.API.Models;
+﻿using Dapper;
+using eCommerce.API.Models;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace eCommerce.API.Repositories
 {
-   
+
     public class UsuarioRepository : IUsuarioRepository
     {
         private IDbConnection _connection;
@@ -14,16 +15,11 @@ namespace eCommerce.API.Repositories
         }
 
 
-        private static List<Usuario> _db = new List<Usuario>()
-        {
-            new Usuario(){Id = 1, Nome = "Axl Rose", Email = "gunsandroses@gmail.com"},
-            new Usuario(){Id = 2, Nome = "Slash", Email = "gunsandroses@gmail.com"},
-            new Usuario{Id = 3, Nome = "Duff Mackgan", Email = "gunsandroses@gmail.com"}
-        };
+
 
         public List<Usuario> Get()
         {
-            return _db;
+            return _connection.Query<Usuario>("select id,nome,email,sexo,rg,cpf,nomeMae,situacaoCadastro,8 from usuarios").ToList();
         }
         public Usuario Get(int id)
         {
@@ -56,6 +52,13 @@ namespace eCommerce.API.Repositories
 
             _db.Remove(_db.FirstOrDefault(x => x.Id == id));
         }
+
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){Id = 1, Nome = "Axl Rose", Email = "gunsandroses@gmail.com"},
+            new Usuario(){Id = 2, Nome = "Slash", Email = "gunsandroses@gmail.com"},
+            new Usuario{Id = 3, Nome = "Duff Mackgan", Email = "gunsandroses@gmail.com"}
+        };
 
     }
 }
