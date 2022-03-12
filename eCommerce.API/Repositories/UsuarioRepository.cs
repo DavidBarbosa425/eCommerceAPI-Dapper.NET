821a62ff2bf01862a07fadb67155885aa475b29a
David <davidbarbosa425@gmail.com>
2022-03-12 18:32:04 -0300

    inserir e atualizar contatos

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -36,18 +36,80 @@ namespace eCommerce.API.Repositories
         }
         public void Insert(Usuario usuario)
         {
-            string sql = "insert into usuarios (nome,email,sexo,rg,cpf,nomeMae,situacaoCadastro,datacadastro)" +
-                "values (@nome,@email,@sexo,@rg,@cpf,@nomeMae,@situacaoCadastro,@datacadastro); select cast(scope_identity() as int);";
+            _connection.Open();
+            var transaction = _connection.BeginTransaction();
 
-            usuario.Id = _connection.Query<int>(sql, usuario).Single();
+            try
+            {
+                string sql = "insert into usuarios (nome,email,sexo,rg,cpf,nomeMae,situacaoCadastro,datacadastro)" +
+                             "values (@nome,@email,@sexo,@rg,@cpf,@nomeMae,@situacaoCadastro,@datacadastro);" +
+                             " select cast(scope_identity() as int);";
+                usuario.Id = _connection.Query<int>(sql, usuario, transaction).Single();
+
+                if (usuario.Contato != null)
+                {
+                    usuario.Contato.UsuarioId = usuario.Id;
+                    string sqlContato = "insert into contatos (usuarioId, telefone,celular) " +
+                        "values(@usuarioId, @telefone,@celular); select cast(scope_identity() as int);";
+                    usuario.Contato.Id = _connection.Query<int>(sqlContato, usuario.Contato, transaction).Single();
+                }
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    //
+                }
+            }
+            finally
+            {
+                
+                _connection.Close();
+            }
+
         }
         public void Update(Usuario usuario)
         {
-            string sql = "update usuarios set nome = @nome ,email = @email,sexo = @sexo,rg = @rg,cpf = @cpf,nomeMae" +
-                " = @nomemae,situacaoCadastro = @situacaocadastro,datacadastro = @datacadastro where id = @id";
+            _connection.Open();
+            var transaction = _connection.BeginTransaction();
 
-            _connection.Execute(sql, usuario);
+            try
+            {
+                string sql = "update usuarios set nome = @nome ,email = @email,sexo = @sexo,rg = @rg,cpf = @cpf,nomeMae" +
+                             " = @nomemae,situacaoCadastro = @situacaocadastro,datacadastro = @datacadastro where id = @id";
+                _connection.Execute(sql, usuario, transaction);
 
+                if (usuario.Contato != null)
+                {
+                    string sqlContato = "update contatos set telefone = @telefone, celular = @celular where id = @id";
+                    _connection.Execute(sqlContato, usuario.Contato, transaction);
+                }
+
+
+
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    transaction.Rollback();
+                }
+                catch (Exception)
+                {
+                    //
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
 
         }
         public void Delete(int id)
