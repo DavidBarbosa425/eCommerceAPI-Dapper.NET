6537c8e647c17c160d99df1f2056372b967dc876
David <davidbarbosa425@gmail.com>
2022-03-15 20:51:54 -0300

    inserir e atualizar endereco de entrega

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -50,16 +50,33 @@ namespace eCommerce.API.Repositories
         }
         public Usuario Get(int id)
         {
-            return _connection.Query<Usuario, Contato, Usuario>("select * from Usuarios u" +
-                " left join Contatos c" +
+            List<Usuario> usuarios = new List<Usuario>();
+
+            string sql = "select * from Usuarios u" +
+                " left join Contatos c " +
                 " on u.id = c.UsuarioId" +
-                " where u.Id = @Id", (usuario, contato) =>
+                " left join EnderecosEntrega ee" +
+                " on ee.UsuarioId = u.Id" +
+                " where u.id = @Id";
+
+            _connection.Query<Usuario, Contato, EnderecoEntrega, Usuario>(sql,
+                (Usuario, Contato, EnderecoEntrega) =>
                 {
-                    usuario.Contato = contato;
-                    return usuario;
-                },
-                new {Id = id}
-           ).SingleOrDefault();
+                    if (usuarios.SingleOrDefault(us => us.Id == us.Id) == null)
+                    {
+                        Usuario.EnderecosEntrega = new List<EnderecoEntrega>();
+                        Usuario.Contato = Contato;
+                        usuarios.Add(Usuario);
+                    }
+                    else
+                    {
+                        Usuario = usuarios.SingleOrDefault(us => us.Id == us.Id);
+                    }
+
+                    Usuario.EnderecosEntrega.Add(EnderecoEntrega);
+                    return Usuario;
+                }, new {Id = id});
+            return usuarios.SingleOrDefault();
         }
         public void Insert(Usuario usuario)
         {
@@ -79,6 +96,16 @@ namespace eCommerce.API.Repositories
                     string sqlContato = "insert into contatos (usuarioId, telefone,celular) " +
                         "values(@usuarioId, @telefone,@celular); select cast(scope_identity() as int);";
                     usuario.Contato.Id = _connection.Query<int>(sqlContato, usuario.Contato, transaction).Single();
+                }
+                if (usuario.EnderecosEntrega != null && usuario.EnderecosEntrega.Count > 0)
+                {
+                    foreach (var enderecoEntrega in usuario.EnderecosEntrega)
+                    {
+                        enderecoEntrega.UsuarioId = usuario.Id;
+                        string sqlEndereco = "insert into enderecosEntrega (usuarioId, nomeEndereco,cep,estado, cidade, bairro,endereco,numero,complemento)" +
+                            "values (@usuarioId, @nomeEndereco,@cep,@estado, @cidade, @bairro,@endereco,@numero,@complemento); select cast(scope_identity() as int);";
+                        enderecoEntrega.Id = _connection.Query<int>(sqlEndereco, enderecoEntrega, transaction).Single();
+                    }
                 }
 
                 transaction.Commit();
@@ -118,7 +145,20 @@ namespace eCommerce.API.Repositories
                     _connection.Execute(sqlContato, usuario.Contato, transaction);
                 }
 
+                string sqlDeletarEndereco = "delete enderecosEntrega where usuarioId = @usuarioId";
+                _connection.Execute(sqlDeletarEndereco, usuario.EnderecosEntrega, transaction);
 
+
+                if (usuario.EnderecosEntrega != null && usuario.EnderecosEntrega.Count > 0)
+                {
+                    foreach (var enderecoEntrega in usuario.EnderecosEntrega)
+                    {
+                        enderecoEntrega.UsuarioId = usuario.Id;
+                        string sqlEndereco = "insert into enderecosEntrega (usuarioId, nomeEndereco,cep,estado, cidade, bairro,endereco,numero,complemento)" +
+                            "values (@usuarioId, @nomeEndereco,@cep,@estado, @cidade, @bairro,@endereco,@numero,@complemento); select cast(scope_identity() as int);";
+                        enderecoEntrega.Id = _connection.Query<int>(sqlEndereco, enderecoEntrega, transaction).Single();
+                    }
+                }
 
                 transaction.Commit();
             }
